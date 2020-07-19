eb6c41a33c59cbef852c44339c2f772e290b3bff
vilsto <g.361acex@gmail.com>
2020-07-20 00:28:30 +0900

    021

--- chap03/021.py
+++ chap03/021.py
@@ -1,15 +1,15 @@
-#20. JSONデータの読み込み
+#21. カテゴリ名を含む行を抽出Permalink
 import pandas as pd
 def chap03_readJsonl(chap03_filename):
     return pd.read_json(chap03_filename, lines= True, compression='infer')
 
 def main():
     chap03_filename= 'jawiki-country.json.gz'
-    df_020= chap03_readJsonl(chap03_filename)
+    df_021= chap03_readJsonl(chap03_filename)
     #クエリで抽出
-    df_UK= df_020.query("title == 'イギリス'")['text'].values[0]
-    #df_UK['text'].values = np.ndarry
-    print(df_UK)
+    df_UK= df_020.query("title == 'イギリス'")
+
+    
 
 if __name__ == "__main__":
     main()
